94930237f7786f936cd7e84449c507383f70b0b4
Eric Mullen <emullen@galois.com>
2017-06-13 13:25:05 -0700

    added some tests

--- src/AST.v
+++ src/AST.v
@@ -3,11 +3,32 @@ Require Import Coq.Arith.PeanoNat.
 
 Definition ident := nat.
 
+Inductive BitV : nat -> Set :=
+| bitNil : BitV O
+| bitCons (b : bool) :
+    forall {n} (bs : BitV n),
+      BitV (S n).
+
+Fixpoint bitlist {n} (b : BitV n) : list bool :=
+  match b with
+  | bitNil => nil
+  | bitCons b bs => b :: bitlist bs
+  end.
+
+Lemma length_correct :
+  forall n (bs : BitV n),
+    length (bitlist bs) = n.
+Proof.
+  induction bs; intros; simpl; auto.
+Qed.
+
 
 Inductive CType := (*TFun (CType n) (CType n)  -- ^ @[8] -> [8]@
             | TSeq (CType n) (CType n)  -- ^ @[8] a@ *)
-| TBit                    (* ^ @Bit@*)
+| TSeq (len : CType) (T : CType) (* Tnum for finite, Tinf for inf, T for type *)
+| TBitv (n : nat)                    (* ^ @Bit@*)
 | TNum (n : nat)            (* ^ @10@*)
+| TInf
 (*            | TChar Char              -- ^ @'a'@
             | TInf                    -- ^ @inf@
             | TUser n [CType n]        -- ^ A type variable or synonym
@@ -21,34 +42,48 @@ Inductive CType := (*TFun (CType n) (CType n)  -- ^ @[8] -> [8]@
               deriving (Eq, Show, Generic, NFData)*)
 .
 
-(* Note: just make a recursive eval_expr relation *)
+(* TODO *)
+(* Add comprehensions *)
+(* Add tests for each Expr *)
 
+Inductive Expr :=
+| EBool (b : bool)
+(* boolean literal, e.g. True *)
+| EVar : ident -> Expr
+(* Variable, e.g. 'x' *)
+| ELit {n} : BitV n -> Expr
+(* Literal, e.g. 0x1200 *)
+| EList : list Expr -> Expr
+(* Literal finite list, e.g. [1,2,3] *)
+| EApp (f v : Expr)
+(* Function application, e.g. f v *)
+| EIf (cond t f : Expr)
+(* If/then/else, e.g. if cond then t else f *)
+| EWhere : Expr -> ident -> Expr -> Expr
+(* Where, e.g. 1 + x where { x = 2 } *)
+| EFun (id : ident) (e : Expr)
+(* Anonymous function, e.g. \\x -> x *)
 
-Inductive Expr := 
-| EVar : ident -> Expr                          (* ^ @ x @ *) 
-| ELit : bool -> Expr                    (* ^ @ 0x10 @ *)
 (*| ETuple [Expr n]                 (* ^ @ (1,2,3) @ *)
-| ERecord [Named (Expr n)]        (* ^ @ { x = 1, y = 2 } @ *)
-| ESel (Expr n) Selector          (* ^ @ e.l @ *)
-| EList [Expr n]                  (* ^ @ [1,2,3] @ *)
-| EFromTo (Type n) (Maybe (Type n)) (Maybe (Type n)) (* ^ @[1, 5 ..  117 ] @ *)
+| ERecord [Named (Expr n)]        (* ^ @ { x = 1, y = 2 } @ *) 
+| ESel (Expr n) Selector          (* ^ @ e.l @ *) *)
+(*| EFromTo (Type n) (Maybe (Type n)) (Maybe (Type n)) (* ^ @[1, 5 ..  117 ] @ *)
 | EInfFrom (Expr n) (Maybe (Expr n))(* ^ @ [1, 3 ...] @ *)
 | EComp (Expr n) [[Match n]]      (* ^ @ [ 1 | x <- xs ] @ *) *)
-| EApp (f v : Expr)          (* ^ @ f x @ *)
 (*| EAppT (Expr n) [(TypeInst n)]   (* ^ @ f `{x = 8}, f`{8} @ *)*)
-| EIf (cond t f : Expr)   (* ^ @ if ok then e1 else e2 @ *) 
-| EWhere : Expr -> ident -> Expr -> Expr        (* ^ @ 1 + x where { x = 2 } @ *)
 (*| ETyped (Expr n) (Type n)        (* ^ @ 1 : [8] @ *)
 | ETypeVal (Type n)               (* ^ @ `(x + 1)@, @x@ is a type *) *)
-| EFun (id : ident) (e : Expr)       (* ^ @ \\x y -> x @ *)
 (*| ELocated (Expr n) Range         (* ^ position annotation *) 
-
 | EParens (Expr n)                (* ^ @ (e)   @ (Removed by Fixity) *)
 | EInfix (Expr n) (Located n) Fixity (Expr n) (* ^ @ a + b @ (Removed by Fixity) *) *)
 .
 
+(* Operational Semantics *)
+
 Inductive val :=
 | bit (b : bool)
+| bits {n} (b : BitV n)
+| seq (l : list val)
 | close (x : ident) (e : Expr) (E : ident -> option val)
 .
 
@@ -64,8 +99,15 @@ Inductive eval_expr : env -> Expr -> val -> Prop :=
       E id = Some v ->
       eval_expr E (EVar id) v
 | eval_lit :
+    forall E n b,
+      eval_expr E (ELit b) (@bits n b)
+| eval_bool :
     forall E b,
-      eval_expr E (ELit b) (bit b)
+      eval_expr E (EBool b) (bit b)
+| eval_list :
+    forall E l vs,
+      Forall2 (eval_expr E) l vs ->
+      eval_expr E (EList l) (seq vs)
 | eval_where :
     forall E exp id bexp bv v,
       eval_expr E bexp bv ->
@@ -91,113 +133,44 @@ Inductive eval_expr : env -> Expr -> val -> Prop :=
       eval_expr (extend E' id v) exp v' ->
       eval_expr E (EApp f a) v'.
 
-      
 
-(*                
+(* Tests *)
 
-Inductive state :=
-| Run (st : list env) (E : env) (e : Expr)
-| Stop (v : val).
+Definition one : BitV (S (S O)).
+  eapply bitCons. exact false.
+  eapply bitCons. exact true.
+  eapply bitNil.
+Defined.
 
+Definition l := EList ((ELit one) :: (ELit one) :: nil).
 
+Lemma eval_list_test :
+  eval_expr empty l (seq ((bits one) :: (bits one) :: nil)).
+Proof.
+  repeat econstructor.
+Qed.
 
+Definition simple_app := EApp (EFun (O) (EVar O)) (EBool true).
 
-
-Inductive step : state -> state -> Prop :=
-| st_var : (* evaluate a variable in an environment *)
-    forall st E id v,
-      E id = Some v ->
-      step (Run st E (EVar id)) (Stop v)
-| st_lit : (* evaluate a literal *)
-    forall st E b,
-      step (Run st E (ELit b)) (Stop (bit b))
-| st_if_cond : (* step the condition *)
-    forall st E c t f c' st' E',
-      step (Run st E c) (Run st' E' c') ->
-      step (Run st E (EIf c t f)) (Run st' E' (EIf c' t f)).
-| st_if_t :
-    forall st E c t f,
-      step (Run (x :: st) E c) (Stop (bit true)) ->
-      step (Run (x :: st) E (EIf c t f)) (Run st x t)
-| st_if_f :
-    forall st E c t f,
-      step (Run st E c) (Stop (bit false)) ->
-      step (Run st E (EIf c t f)) (Run st E f)
-| st_where_r :
-    forall st E id_expr id_expr' expr id,
-      step (Run st E id_expr) (Run st E id_expr') ->
-      step (Run st E (EWhere expr id id_expr)) (Run st E (EWhere expr id id_expr'))
-| st_where_v :
-    forall st e id_expr expr id,
-      step (Run st e id_expr) (Stop v) ->
-      step (Run st e (EWhere expr id id_expr)) (Run (e :: st) (extend e id v) (EPop expr))
-| st_pop :
-    
-
-(* 
-
-if (x where x = false) then 1 else 0
+Lemma eval_app_test :
+  eval_expr empty simple_app (bit true).
+Proof.
+  repeat econstructor.
+Qed.
 
 
-*)
-    
+Definition btest := EIf (EBool true) (EBool false) (EVar O).
 
+Lemma btest_eval :
+  eval_expr empty btest (bit false).
+Proof.
+  repeat econstructor.
+Qed.
 
+Definition wtest := EWhere (EVar O) O (ELit one).
 
-Definition step (st : state) : option state :=
-  match st with
-  | Run s E e =>
-    match e with
-    | ELit m => Some (Stop (bit m))
-    | EVar id =>
-      match E id with
-      | Some v => Some (Stop v)
-      | None => None
-      end
-    | EWhere exp id rhs =>
-      
-      match eval E rhs with
-      | Some rhv =>
-        eval (extend E id rhv) exp
-      | None => None
-      end
-    | EIf cond t f =>
-      match eval E cond with
-      | Some (bit true) => eval E t
-      | Some (bit false) => eval E f
-      | _ => None
-      end
-        
-  
-
-Fixpoint eval (E : env) (e : Expr) : option val :=
-  match e with
-  | ELit m => Some (bit m)
-  | EVar id => E id
-  | EWhere exp id rhs =>
-    match eval E rhs with
-    | Some rhv =>
-      eval (extend E id rhv) exp
-    | None => None
-    end
-  | EIf cond t f =>
-    match eval E cond with
-    | Some (bit true) => eval E t
-    | Some (bit false) => eval E f
-    | _ => None
-    end
-  | EFun id e =>
-    Some (close id e E)
-  | EApp f v =>
-    match eval E f with
-    | Some (close id e E') =>
-      match eval E v with
-      | Some v' =>
-        eval (extend E' id v') e
-      | None => None
-      end
-    | _ => None
-    end
-  end.
-
-*)
+Lemma wtest_eval :
+  eval_expr empty wtest (bits one).
+Proof.
+  repeat econstructor.
+Qed.
