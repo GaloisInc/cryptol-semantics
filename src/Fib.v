90c35fc83b763cb68090101d4b7425acdf346458
Nathan Launchbury <nate@whodat-41-231.galois.com>
2017-06-22 14:57:43 -0700

    fibonacci in coq and from cryptol

--- src/Fib.v
+++ src/Fib.v
@@ -21,14 +21,18 @@ Require Import Semantics.
 Require Import Utils.
 Require Import Builtins.   
         
+(* Grab some useful tactics : 
+  e: applies econstructor and some simplification whenever there is no choice to be made (not an If or Var) 
+  global: resolves the constructor for a var in the global env
+  localL resolves the constructor for a var in the local env  *)
 Require Import Tactics. 
 
 (* right side of this generated from cryptol implementation *)
 
 Import HaskellListNotations.
 
-(* right side of this generated from cryptol implementation *)
 
+(* right side of this generated from cryptol implementation Fib.cry *)
 Definition fib_cry : DeclGroup := (Recursive [(Decl 484 (DExpr (EAbs 485 (EIf (EApp (EApp (ETApp (EVar 259) (TCon (TC TCSeq) [TCon (TC (TCNum 32)) [],TCon (TC TCBit) []])) (EVar 485)) (ETApp (ETApp (EVar 242) (TCon (TC (TCNum 0)) [])) (TCon (TC (TCNum 32)) []))) (ETApp (ETApp (EVar 242) (TCon (TC (TCNum 1)) [])) (TCon (TC (TCNum 32)) [])) (EIf (EApp (EApp (ETApp (EVar 259) (TCon (TC TCSeq) [TCon (TC (TCNum 32)) [],TCon (TC TCBit) []])) (EVar 485)) (ETApp (ETApp (EVar 242) (TCon (TC (TCNum 1)) [])) (TCon (TC (TCNum 32)) []))) (ETApp (ETApp (EVar 242) (TCon (TC (TCNum 1)) [])) (TCon (TC (TCNum 32)) [])) (EApp (EApp (ETApp (EVar 243) (TCon (TC TCSeq) [TCon (TC (TCNum 32)) [],TCon (TC TCBit) []])) (EApp (EVar 484) (EApp (EApp (ETApp (EVar 243) (TCon (TC TCSeq) [TCon (TC (TCNum 32)) [],TCon (TC TCBit) []])) (EVar 485)) (EApp (ETApp (EVar 253) (TCon (TC TCSeq) [TCon (TC (TCNum 32)) [],TCon (TC TCBit) []])) (ETApp (ETApp (EVar 242) (TCon (TC (TCNum 1)) [])) (TCon (TC (TCNum 32)) [])))))) (EApp (EVar 484) (EApp (EApp (ETApp (EVar 243) (TCon (TC TCSeq) [TCon (TC (TCNum 32)) [],TCon (TC TCBit) []])) (EVar 485)) (EApp (ETApp (EVar 253) (TCon (TC TCSeq) [TCon (TC (TCNum 32)) [],TCon (TC TCBit) []])) (ETApp (ETApp (EVar 242) (TCon (TC (TCNum 2)) [])) (TCon (TC (TCNum 32)) [])))))))))))]).
 
 Definition width : nat := 32. 
@@ -41,9 +45,9 @@ Proof.
   unfold width. congruence. 
 Qed.
 
-(* 17 -> eq *)
-(* 1 -> plus *)
-(* 11 -> neg *)
+(* 242 -> plus *)
+(* 259 -> eq *)
+(* 243 -> neg *)
 Definition plus_decl := builtin_binop 242 Plus.
 Definition eq_decl := builtin_binop 259 Eq.
 Definition neg_decl := builtin_unop 243 Neg.
@@ -58,28 +62,6 @@ Definition fib_ge := bind_decl_group fib_cry
 
 (* Local environment *)
 Definition Env (n:Z) := extend empty fibvar (bits (@repr width nz n)). 
-
-(*
-
-Function fib (n : nat) : nat := 
-  match n with 
-   | O => 1
-   | S n' => match n' with
-        | O => 1
-        | S n'' => fib n' + fib n''
-   end 
-end.
-
-
-
-Theorem fib_equiv : forall n m, m = fib (Z2N n) ->  
-   eval_expr fib_ge (Env n) (EApp (EVar fibnum) (EVar fibvar)) 
-   (bits (@repr width nz m)). 
-
-*)
-
-
-
 
 
 
@@ -113,7 +95,7 @@ Proof.
   e. 
   - global. e. 
   - local. 
-  - eapply eval_if_f. 
+  - eapply eval_if_f.                 (* Doesn't match first base case *)
     + e. e. e. global. e. local.  
       * unfold extend. e. 
       * e. 
@@ -121,7 +103,7 @@ Proof.
         -- local. 
         -- local.   
         -- constructor; exact nz.  
-    + eapply eval_if_t. 
+    + eapply eval_if_t.               (* Matches second base case *)
       * e. e. e. global. e. local.  
         -- unfold extend. e. 
         -- e. 
@@ -130,11 +112,20 @@ Proof.
            ++ local.   
            ++ constructor; exact nz.
       * e.  
-  Unshelve. exact nz. exact nz. 
+  Unshelve. all: exact nz. 
 Qed.    
-  
 
 Definition E2 := Env 2. 
+
+Lemma plus_fib12s : (eval_expr fib_ge E2 (EApp (EVar 242) (EApp (EVar fibvar) (EVar fibvar))) (bits (@repr width nz 24))).
+Proof. 
+  e. 
+  - global. e. 
+  - e. 
+    + eapply eval_global_var. unfold E2. unfold Env. unfold extend. simpl. 
+  
+Lemma plus_f0_f1 : eval_expr fib_ge E2 (EApp (EVar 242) (eval_expr fib_ge E (EApp (EVar fibnum) (EVar fibvar))) (eval_expr fib_ge E1 (EApp (EVar fibnum) (EVar fibvar)))) (bits (@repr width nz 0)).  
+
 
 (* Fib 2 = 2 *)
 Lemma eval_fib2 : eval_expr fib_ge E2 (EApp (EVar fibnum) (EVar fibvar)) (bits (@repr width nz 2)).
@@ -142,7 +133,7 @@ Proof.
   e. 
   - global. e. 
   - local. 
-  - eapply eval_if_f. 
+  - eapply eval_if_f.                  (* Doesn't match first base case *)
     + e. e. e. global. e. local.  
       * unfold extend. e. 
       * e. 
@@ -150,7 +141,7 @@ Proof.
         -- local. 
         -- local.   
         -- constructor; exact nz. 
-    + eapply eval_if_f. 
+    + eapply eval_if_f.                (* Doesn't match second base case *)
       * e. e. e. global. e. local.  
         -- unfold extend. e. 
         -- e. 
@@ -158,11 +149,13 @@ Proof.
           ++ local. 
           ++ local.   
           ++ constructor; exact nz.
-      * econstructor. 
+      * econstructor.                  (* Take recursive case *)
       (* Something is going wrong in the next line *)
       e. e. e. global. e. e. global. e. e. e. e. global. e. local. e. local. 
         -- (* This isn't true - eapply eval_neg.*) 
 Admitted. 
+
+Definition E3 := Env 3. 
 
 (* Fib 3 = 3 *)
 Lemma eval_fib3 : eval_expr fib_ge E3 (EApp (EVar fibnum) (EVar fibvar)) (bits (@repr width nz 3)).
@@ -174,8 +167,9 @@ Proof.
     + econstructor. 
   - econstructor. unfold E. unfold extend. simpl. reflexivity. 
   - 
+Admitted. 
 
-
+(*
  (* #3 *)unfold fib_ge. simpl. 
     econstructor. 
     econstructor. 
@@ -204,7 +198,7 @@ Proof.
         unfold extend. simpl. unfold empty. (* This isn't true. So E doesn't have everything we need (?) *)  
 
 
-
+*)
 
 
 
--- src/FibEquiv.v
+++ src/FibEquiv.v
@@ -9,7 +9,9 @@ Require Import AST.
 Require Import Semantics.
 Require Import Utils.
 Require Import Builtins.
+Require Import Tactics.  
         
+Require Import ZArith.Znat.  
 
 Import HaskellListNotations.
 
@@ -20,18 +22,60 @@ Definition nz32 (n:Z) := @repr 32 nz n.
 Definition add32 (x y : Int) := @add 32 nz x y. 
 Definition sub32 (x y : Int) := @sub 32 nz x y. 
 
+
 (*
+Writing a Fib function over 32bit Ints doesn't appear to be fruitful since Coq doesn't believe that it terminates (since it doesn't for any negative argument). 
+
 Fixpoint fib (n : @Int 32) : (@Int 32) := 
   if (eq n (nz32 0)) then (nz32 1)
   else if (eq n (nz32 1)) then (nz32 1)
-  else add32 (fib (sub32 n (nz32 2))) (fib (sub32 n (nz32 1))). 
+  else add32 (fib (add32 n (nz32 (-1)))) (fib (add32 n (nz32(-2)))). 
 
-
-
-Theorem fib_equiv : forall n,
-   eval_expr fib_ge (Env n) (EApp (EVar fibnum) (EVar fibvar)) 
-   (bits (@repr width nz (fib n)). 
-
+Instead we will just define Fib over nats and then convert the result to a 32bit Int. Our equivalence theorem will have to have a restriction on the size of the input to take overflow into account
 *)
+
+Fixpoint fib (n : nat) : nat :=
+  match n with
+  | O  => S O 
+  | S n' => match n' with 
+         | O => S O
+         | S n'' => fib n' + fib n''
+  end
+end.
+
+(* Example of converting a fib result to 32bit Int *)
+Eval compute in (nz32 (Z.of_nat (fib 5))).   
+
+(* For the condition of the equiv theorem *)
+Definition Is_true (b:bool) :=
+  match b with
+    | true => True
+    | false => False
+  end.
+
+(* Just get the intval field in the Int record *)
+Definition Int32_to_Z (n : Int) : Z := @intval 32 n. 
+
+(* Lots of type conversion to say "Fib.cry n = fib n" *)
+Theorem fib_equiv : forall n, Is_true (ltu n (nz32 (@max_unsigned 32))) -> 
+   eval_expr fib_ge (Env (Int32_to_Z n)) (EApp (EVar fibnum) (EVar fibvar)) 
+   (bits (nz32 (Z.of_nat (fib (Z.to_nat (Int32_to_Z n)))))). 
+Proof. 
+  intros.  
+  e.
+  - global. e.
+  - local. 
+  - induction (Z.to_nat (Int32_to_Z n)). (* induction on n as a nat *)
+    + eapply eval_if_t. 
+    e. e. e. global. e. local. e. e. e. local. local.
+      * constructor; exact nz.
+      * e. 
+    + econstructor. e. e. e. global. e. local. e. e. e. local. local.
+      * destruct p. (* these aren't all true. Probably chose a constructor too early (eval_if_t) *)
+
+
+
+
+
 
 
