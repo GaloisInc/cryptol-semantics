dc8533b3ba33d5a3bb22913f175ea1d6a3f3a3a5
Nathan Launchbury <nate@whodat-41-3.galois.com>
2017-07-13 14:59:34 -0700

    beginning of one-time pad

---
+++ verif/OTP_verif.v
@@ -0,0 +1,40 @@
+Require Import List.
+Import ListNotations.
+Require Import String.
+
+(* Borrow from CompCert *)
+Require Import Coqlib.
+Require Import Bitvectors.
+
+Require Import AST.
+Require Import Semantics.
+Require Import Utils.
+Require Import Builtins.
+Require Import BuiltinSem.
+Require Import Values.        
+Require Import Bitstream.
+
+Require Import EvalTac.
+Require Import Eager.
+
+Import HaskellListNotations.
+Open Scope string.
+
+Require Import OTP. 
+
+
+
+Function otp_encrypt (k : BitV 8) (msg : BitV 8) : BitV 8 :=
+  xor k msg.
+
+Eval compute in (otp_encrypt (@repr 8 8) (@repr 8 5)). 
+Eval compute in (otp_encrypt (@repr 8 8) (@repr 8 13)). 
+
+Print strictval. 
+
+(*Lemma otp_equiv : forall k msg bvk bvm, 
+  to_bitv k = Some bvk -> 
+  to_bitv msg = Some bvm -> 
+  strict_eval_expr ge empty (EApp (EApp (EVar encrypt) (EList (map EValue k))) (EList (map EValue msg))) (otp_encrypt bvk bvm). 
+*)
+
