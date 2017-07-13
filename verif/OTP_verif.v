5887e6a4c179c39956b9ad551ae8e1edcaab2ff3
Nathan Launchbury <nate@whodat-41-3.galois.com>
2017-07-13 16:37:06 -0700

    beginning of OTP proof

--- verif/OTP_verif.v
+++ verif/OTP_verif.v
@@ -1,3 +1,4 @@
+Add LoadPath "~/Desktop/Galois/cryptol-semantics/verif".
 Add LoadPath "~/Desktop/Galois/cryptol-semantics/src".
 Require Import List.
 Import ListNotations.
@@ -21,21 +22,38 @@ Require Import Eager.
 Import HaskellListNotations.
 Open Scope string.
 
-(*Require Import OTP.*) 
+Require Import OTP. 
 
 
 
-Function otp_encrypt (k : BitV 8) (msg : BitV 8) : BitV 8 :=
+Definition otp_encrypt (k : BitV 8) (msg : BitV 8) : BitV 8 :=
   xor k msg.
+  
+Definition otp_encrypt'  (k : list val) (m : list val) : list val :=
+  match to_bitv k with
+  | None => []
+  | Some bvk => 
+      match to_bitv m with
+      | None => []
+      | Some bvm => from_bitv (otp_encrypt bvk bvm)  
+      end
+  end. 
 
-(* Eval compute in (otp_encrypt (@repr 8 8) (@repr 8 5)). 
-Eval compute in (otp_encrypt (@repr 8 8) (@repr 8 13)). 
-*)
+Definition sempty : senv := fun _ => None.  
 
 
-(*Lemma otp_equiv : forall k msg bvk bvm, 
-  to_bitv k = Some bvk -> 
-  to_bitv msg = Some bvm -> 
-  strict_eval_expr ge empty (EApp (EApp (EVar encrypt) (EList (map EValue k))) (EList (map EValue msg))) (otp_encrypt bvk bvm). 
-*)
+
+
+
+Lemma otp_equiv : forall k msg l, 
+  strict_eval_val ge (thunk_list (otp_encrypt' k msg)) l ->
+  eager_eval_expr ge sempty (EApp (EApp (EVar encrypt) (EList (map EValue k))) (EList (map EValue msg))) l. 
+Proof.
+Admitted. 
+
+
+
+
+
+
 
