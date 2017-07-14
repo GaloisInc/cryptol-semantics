0d3713fca0101cb3e1995c0de472bdb9aa43502c
Nathan Launchbury <nate@whodat-41-3.galois.com>
2017-07-14 13:16:31 -0700

    started proof of encrypt equivalence

--- verif/OTP_verif.v
+++ verif/OTP_verif.v
@@ -26,10 +26,10 @@ Require Import OTP.
 
 
 
-Definition otp_encrypt (k : BitV 8) (msg : BitV 8) : BitV 8 :=
+Definition otp_encrypt (k msg : BitV 8) : BitV 8 :=
   xor k msg.
   
-Definition otp_encrypt'  (k : list val) (m : list val) : list val :=
+Definition otp_encrypt'  (k m: list val) : list val :=
   match to_bitv k with
   | None => []
   | Some bvk => 
@@ -41,14 +41,56 @@ Definition otp_encrypt'  (k : list val) (m : list val) : list val :=
 
 Definition sempty : senv := fun _ => None.  
 
-
+(*Lemma something : forall k ge e, 
+  exists k', 
+  Forall2 (eager_eval_expr ge e) (map EValue k) k'. 
+Proof.
+  induction k; intros.
+  - eexists. econstructor.
+  - edestruct IHk; eauto.
+    eexists. econstructor; eauto.
+    econstructor.    
+ *) 
 
 
 
 Lemma otp_equiv : forall k msg l, 
+n_bits 8 k -> 
+n_bits 8 msg -> 
   strict_eval_val ge (thunk_list (otp_encrypt' k msg)) l ->
   eager_eval_expr ge sempty (EApp (EApp (EVar encrypt) (EList (map EValue k))) (EList (map EValue msg))) l. 
-Proof.
+Proof. 
+  intros. eapply n_bits_eval in H. destruct H. eapply n_bits_eval in H0. destruct H0.    
+  econstructor. econstructor. eapply eager_eval_global_var. 
+  unfold sempty. reflexivity.
+  unfold ge. econstructor. econstructor. 
+  eassumption.   
+  econstructor. eassumption. 
+
+  econstructor.
+  econstructor. econstructor. eapply eager_eval_global_var. unfold extend. simpl. unfold sempty. reflexivity.
+  unfold ge. econstructor.
+  econstructor. econstructor. econstructor. eauto.
+  econstructor. econstructor. econstructor. econstructor. econstructor.
+  econstructor. eapply eager_eval_local_var. unfold extend. simpl. eauto.
+  econstructor.
+  econstructor. econstructor. unfold extend. simpl. eauto.
+  econstructor.
+  
+  econstructor. unfold extend. simpl. auto.
+  econstructor. econstructor. unfold extend. simpl. auto.
+  econstructor.
+  
+  simpl. 
+  inversion H. subst. clear H.  
+  inversion H0. subst. clear H0.
+         
+
+  
+     
+      
+
+
 Admitted. 
 
 
