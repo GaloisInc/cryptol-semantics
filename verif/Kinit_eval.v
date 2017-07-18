7d65be8e3af1c152586d73306844333b4ae97ee4
Eric Mullen <emullen@galois.com>
2017-07-17 17:17:32 -0700

    made Kinit proof more polymorphic

--- verif/Kinit_eval.v
+++ verif/Kinit_eval.v
@@ -49,40 +49,72 @@ Definition pwBytes := typenum 64.
 Definition blockLength := typenum 64.
 Definition digest := typenum 0.
 
+Ltac find_rw :=
+  match goal with
+  | [ H : ?X = _ |- ?X = _ ] => rewrite H
+  end.
+
+
 
 Lemma kinit_eval :
   forall k,
     n_bits 64 k ->
-    forall h,
+    forall h TE SE GE,
+      GE kinit = ge kinit ->
+      GE (14, ">") = ge (14,">") ->
+      GE (0, "demote") = ge (0, "demote") ->
+      GE (61, "take") = ge (61, "take") ->
+      GE (35, "splitAt") = ge (35, "splitAt") ->
+      GE (29, "zero") = ge (29, "zero") ->
+      GE (34, "#") = ge (34, "#") ->
+      SE kinit = None ->
+      SE (14, ">") = None ->
+      SE (34, "#") = None ->
+      SE (0, "demote") = None ->
+      SE (29, "zero") = None ->
+      SE (61, "take") = None ->
+      SE (35, "splitAt") = None ->
       (exists hv,
-          eager_eval_expr ge tempty sempty h hv) ->
+          eager_eval_expr GE TE SE h hv) ->
     exists v,
-      eager_eval_expr ge tempty sempty (apply (tapply (EVar kinit) (pwBytes :: blockLength :: digest ::  nil)) (h :: (EList (map EValue k)) :: nil)) v /\ eager_eval_expr ge tempty sempty (EList (map EValue k)) v.
+      eager_eval_expr GE TE SE (apply (tapply (EVar kinit) (pwBytes :: blockLength :: digest ::  nil)) (h :: (EList (map EValue k)) :: nil)) v /\ eager_eval_expr GE TE SE (EList (map EValue k)) v.
 Proof.
   init_globals ge.
   intros.
   remember H as Hnb; clear HeqHnb.
   eapply n_bits_eval in H; eauto.
-  destruct H. instantiate (3 := ge) in H.
-  instantiate (1 := sempty) in H.
-  instantiate (1 := tempty) in H.
-  destruct H.
+  match goal with
+  | [ H : exists _, _ |- _ ] => destruct H
+  end.
+  instantiate (3 := GE) in H.
+  instantiate (1 := SE) in H.
+  instantiate (1 := TE) in H.
+  match goal with
+  | [ H : _ /\ _ |- _ ] => destruct H
+  end.
   inversion H. subst.
-  destruct H0.
+  match goal with
+  | [ H : exists _, _ |- _ ] => destruct H
+  end.
   eexists. split; try eassumption. unfold apply.
   
   e. e.
   e. e. e.
   
-  g.
+  g. find_rw.
+  reflexivity.
   e. e. repeat e.
   e. repeat e.
   e. repeat e.
   eassumption.
-  e. 
+  e. e. 
   eassumption.
-  e. e. e. e. g. e. repeat e.
-  e. e. e. g. e.
+  e. e. e. e. g.
+  find_rw. eassumption.
+  e. repeat e.
+  e. e. e. g.
+  find_rw. eassumption.
+  e.
   repeat e. repeat e.
   simpl. repeat e.
   simpl. e. repeat e.
@@ -90,33 +122,51 @@ Proof.
   
 
   
-  e. e. e. fg. repeat e.
-  e. repeat e. e. repeat e. repeat e.
-  repeat e. repeat e.
-  simpl.
-
+  e. e. e.
+  g.
+  repeat e.
+  find_rw. eassumption.
+  e. repeat e. e. repeat e.
+  e. repeat e.
+  repeat e. simpl. reflexivity.
+  repeat e.
+  
+  
   e. e. e. e. g.
+  find_rw. reflexivity.
   repeat e. repeat e.
   e. repeat e.
   e. repeat e.
   e. e. e. e. e. e. g.
-  repeat e. e. e. repeat e.
-  e. repeat e. e. e. e. e. g.
-  repeat e. repeat e.
-  repeat e. repeat e.
+  repeat e.
+  find_rw. eassumption.
+  e. e. repeat e.
+  e. repeat e. e. e. e. e. e.
+  e. e.
+  g. find_rw. eassumption.
+  e. repeat e. e. repeat e. repeat e.
+  reflexivity. e. e. repeat e.
+  repeat e. repeat e. 
 
+  
   rewrite append_strict_list. reflexivity.  
   
   e. g. e. g.
   e. e. e. e. g.
-  all: try solve [repeat e].
-  e. repeat e. repeat e.
-
-
+  
+  simpl. unfold extend. simpl.
+  find_rw. eassumption.
+  e. e. e. e. e. e. e. e. e. e.
+  e. e. e. e. e. e. e. e. e.
+  
   replace (tnum 64) with (tnum (Z.of_nat (Datatypes.length x))).
+  eapply strict_list_injective in H20. subst.
   rewrite splitAt_len. reflexivity.
   
   f_equal. omega.
-  simpl. reflexivity.
+  simpl.
+  eapply strict_list_injective in H20; eauto.
+  congruence.
 
 Qed.
+
