2a544ba766c5b3dd4174a8eaadf3799885064ba1
Eric Mullen <emullen@galois.com>
2017-07-28 14:15:11 -0700

    proof is coming along

--- verif/HMAC_specs_equiv.v
+++ verif/HMAC_specs_equiv.v
@@ -74,22 +74,6 @@ Proof.
   econstructor; eauto.
 Qed.
 
-(* TODO *)
-Definition correct_model_hash {c p : nat} (hf : ext_val -> ext_val) (h : list (Bvector (b c p)) -> Bvector c) : Prop := True. 
-
-
-(*Check HMAC.*)
-(* HMAC args in order: c p : nat (bit widths) *)
-(* Bv c -> Bv (c + p) -> Bv c: "compression function", i.e. the hash *)
-(* iv : Bv c "initialization vector" *)
-(* msgtype : Set *)
-(* splitAndPad: turn message into bits *)
-(* fpad : Bvector c -> Bvector (c + p) pads on 0s *)
-(* opad ipad : more 0s *)
-(* key : Bv (c + p) *)
-(* msg : Message *)
-(* result has type Bv c *)
-
 Fixpoint bv_to_extval' {w : nat} (bv : Bvector w) : list ext_val :=
   match bv with
   | Vector.nil => nil
@@ -100,30 +84,31 @@ Definition bv_to_extval {w : nat} (bv : Bvector w) : ext_val :=
   let bits := bv_to_extval' bv in
   let bytes := get_each_n 8 bits in
   eseq (map eseq bytes).
-(*
-Definition get_byte (e : ext_val) : option (list bool) :=
-  match e with
-  | eseq l =>
-    match l with
-    | (ebit b1 :: ebit b2 :: ebit b3 :: ebit b4 ::
-            ebit b5 :: ebit b6 :: ebit b7 :: ebit b8 :: nil) => Some ([b1;b2;b3;b4;b5;b6;b7;b8])
-    | _ => None
+
+(* Bottom out with nil, maybe not a great idea but it's ok *)
+Fixpoint eappend (l : list ext_val) : ext_val :=
+  match l with
+  | nil => eseq nil
+  | (eseq l0) :: r =>
+    match eappend r with
+    | eseq x => eseq (l0 ++ x)
+    | _ => eseq nil
     end
-  | _ => None
+  | _ => eseq nil
   end.
 
-(* take a list of bytes as an ext_val, convert to flattened bits  *)
-Definition extval_to_bv' (e : ext_val) : option (list bool) :=
-  match e with
-  | eseq l =>
-    match collect (map get_byte l) with
-    | Some bytes =>
-      Some ((fold_left (@app bool) bytes nil))
-    | _ => None
-    end
-  | _ => None
-  end.
-*)
+
+(* TODO *)
+Definition correct_model_hash {c p : nat} (hf : ext_val -> ext_val) (h : list (Bvector (b c p)) -> Bvector c) : Prop := True. 
+
+(* We need this to be true about the hash function *)  
+Axiom correct_hash_commutes :
+  forall {c p : nat} hf (HASH : list (Bvector (b c p)) -> Bvector c),
+    correct_model_hash hf HASH ->
+    forall l,
+      bv_to_extval (HASH l) = hf (eappend (map bv_to_extval l)).
+
+
 Lemma split_append :
   forall a (x : Bvector a) b (y : Bvector b),
     splitVector a b (Vector.append x y) = (x,y).
@@ -133,31 +118,109 @@ Proof.
   simpl. rewrite IHx. reflexivity.
 Qed.
 
+Lemma get_each_n_head :
+  forall {A : Type} (l l' : list A) n,
+    n = length l ->
+    n <> O ->
+    get_each_n n (l ++ l') = [l] ++ get_each_n n l'.
+Proof.
+Admitted.
 
-(*
-Definition extval_to_bv {w : nat} (e : ext_val) :=
-  match extval_to_bv' e with
-  | Some l =>
-    match Nat.eq_dec (length l) w with
-    | left p => Some (Vector.of_list l)
-    | _ => None
-    end
-  | None => None
-  end.
-*)
 
+Lemma hmac_first_part_equiv :
+  forall keylen w ekey nkey opad,
+    has_type (eseq ekey) (bytestream keylen) ->
+    bv_to_extval nkey = eseq ekey ->
+    (* opad = 92 *)
+    map eseq (get_each_n 8 (bv_to_extval' (BVxor w nkey opad))) = map (xor_const 92) ekey.
+Proof.
+  induction keylen; intros.
+  * unfold bytestream in H.
+    inversion H. subst. destruct ekey; simpl in H2; try congruence.
+    simpl. destruct nkey; simpl in H0; try congruence.
+    Focus 2. unfold bv_to_extval in H0. inversion H0.
+    unfold bv_to_extval'.
+    assert (BVxor 0 []%vector opad = []%vector) by admit.
+    rewrite H1. simpl. reflexivity.
+
+  * inversion H.
+    destruct ekey; simpl in H2; try congruence.
+    destruct nkey.
+    unfold bv_to_extval in H0.
+    unfold bv_to_extval' in H0. simpl in H0. inversion H0.
+    inversion H3.
+    unfold bv_to_extval in H0.
+    unfold bv_to_extval' in H0.
+    fold (@bv_to_extval' n) in H0.
+    inversion H7. subst.
+    do 9 (destruct l1; simpl in H9; try congruence).
+    clear H9.
+    repeat match goal with
+           | [ H : Forall (fun x => has_type x tbit) _ |- _ ] => inversion H; clear H
+           end;
+      repeat match goal with
+             | [ H : has_type _ tbit |- _ ] => inversion H; clear H
+             end;
+    subst.
+    
+    do 7 (destruct nkey; [ subst;
+    unfold bv_to_extval in H0;
+    unfold bv_to_extval' in H0; simpl in H0; inversion H0;
+    inversion H3 | 
+                     unfold bv_to_extval in H0;
+                     unfold bv_to_extval' in H0;
+                     fold (@bv_to_extval' n) in H0 ]).
+    remember (bv_to_extval' nkey) as extval_key.
+
+    replace ((ebit h :: ebit h0 :: ebit h1 :: ebit h2 :: ebit h3 :: ebit h4 :: ebit h5 :: ebit h6 :: extval_key)) with
+        ((ebit h :: ebit h0 :: ebit h1 :: ebit h2 :: ebit h3 :: ebit h4 :: ebit h5 :: ebit h6 :: nil) ++ extval_key) in H0 by reflexivity.
+      
+    erewrite get_each_n_head in H0 by (simpl; eauto).
+    simpl in H0.
+    inversion H0. subst.
+    clear H0.
+    inversion H2.
+    unfold length.
+
+    eapply is_seq in H8.
+    replace (tseq (length (map eseq (get_each_n 8 (bv_to_extval' nkey)))) byte) with
+        (bytestream (length (map eseq (get_each_n 8 (bv_to_extval' nkey))))) in H8 by reflexivity.
+
+    rewrite H1 in H8.
+    eapply IHkeylen in H8.
+
+    Focus 2. 
+    unfold bv_to_extval. reflexivity.
+
+    (* This needs destruction of opad along with nkey *)
+    
+Admitted.
+
+
+Lemma hmac_second_part_equiv :
+  forall c p hf HASH,
+    correct_model_hash hf HASH ->
+    forall ekey emsg l,
+      hf (eseq (map (fun x : ext_val => xor_const 54 x) ekey ++ emsg)) = eseq l ->
+      forall (fpad : Bvector c -> Bvector p) ipad nkey msgl,
+        bv_to_extval' (Vector.append (HASH (BVxor (b c p) nkey ipad :: msgl)) (fpad (HASH (BVxor (b c p) nkey ipad :: msgl)))) = l.
+Proof.
+Admitted.
 
 (* I think this is the right theorem *)
 Theorem HMAC_equiv (MSGT : Set) :
   forall keylen msglen key msg,
     has_type key (bytestream keylen) ->
     has_type msg (bytestream msglen) ->
-    forall c p hf res HashBlock iv (splitAndPad : MSGT -> list (Bvector (b c p))) fpad opad ipad,
+    forall c p hf res HashBlock iv (splitAndPad : MSGT -> list (Bvector (b c p))) fpad,
       hmac_model hf key msg = Some res ->
       @correct_model_hash c p hf (h_star p HashBlock iv) ->
       forall nmsg nkey,
         eseq (map bv_to_extval (splitAndPad nmsg)) = msg ->
         bv_to_extval nkey = key ->
+        forall opad ipad,
+          (* opad = 92 = 0x5C *)
+          (* ipad = 54 = 0x36 *)
         bv_to_extval (@HMAC c p HashBlock iv MSGT splitAndPad fpad opad ipad nkey nmsg) = res.
 Proof.
   intros.
@@ -185,19 +248,17 @@ Proof.
   Focus 2. simpl. reflexivity.
   remember (h_star p HashBlock iv) as HASH.
 
-  
-  
-  (* TODO: What's the correct version of this? *)
-Lemma correct_hash_commutes :
-  forall {n m} h (hf : Bvector n -> Bvector (b n m) -> Bvector n) iv,
-    correct_model_hash h hf iv ->
-    forall x y,
-      @bv_to_extval n (hf x y) = h (@bv_to_extval (b n m) y).
-Proof.
-Admitted.
+  rename l into ekey.
+  rename l0 into emsg.
+  erewrite correct_hash_commutes by eassumption.
+  f_equal. simpl.
+  f_equal. rewrite app_nil_r.
+  f_equal.
+  eapply hmac_first_part_equiv; eauto.
 
+  f_equal. f_equal.
 
-
+  remember (splitAndPad nmsg) as msgl.
   
-  
-Admitted.
+  eapply hmac_second_part_equiv; eauto.
+Qed.
