3d94d83d96519c64c314cf26944692d6219ed52f
Eric Mullen <emullen@galois.com>
2017-08-03 13:07:30 -0700

    proved a simple lemma about get_each_n

--- verif/HMAC_lib.v
+++ verif/HMAC_lib.v
@@ -702,12 +702,85 @@ Proof.
 Qed.
 
 
+Lemma get_each_n'_zero :
+  forall {A} fuel (l : list A),
+    l <> nil ->
+    get_each_n' fuel O l = repeat [] fuel.
+Proof.
+  induction fuel; intros.
+  simpl. destruct l; reflexivity.
+  simpl. destruct l; try congruence.
+  erewrite IHfuel; eauto.
+Qed.
+
+Lemma map_repeat_nil :
+  forall {A B} k (f : A -> B),
+    repeat [] k = map (map f) (repeat [] k).
+Proof.
+  induction k; intros; simpl; auto.
+  f_equal. auto.
+Qed.
+
+Lemma length_list_drop :
+  forall {A} (l : list A) n,
+    (n > O)%nat ->
+    l <> nil ->
+    (Datatypes.length (list_drop n l) < Datatypes.length l)%nat.
+Proof.
+  induction l; intros.
+  destruct n; simpl; try omega; congruence.
+  simpl.
+  destruct n; try omega.
+  simpl.
+  destruct n; try solve [simpl; auto].
+  specialize (IHl (S n)).
+  assert (l = nil \/ l <> []) by (destruct l; try solve [left; congruence]; right; congruence).
+  destruct H1. subst l; simpl; auto.
+  assert (S n > O)%nat by omega.
+  eapply IHl in H2; try eapply H1. omega.
+Qed.
+
+Lemma get_each_n'_map_commutes_strong :
+  forall {A B : Type} (f : A -> B) len k l fuel n,
+    (len >= Datatypes.length l)%nat ->
+    (k >= fuel)%nat ->
+    get_each_n' fuel n (map f l) = map (map f) (get_each_n' fuel n l).
+Proof.
+  induction len; induction k; intros.
+  assert (fuel = O) by omega. subst. simpl.
+  destruct l; reflexivity.
+  destruct l; simpl in H; try omega.
+  simpl. destruct fuel; simpl; reflexivity.
+  destruct fuel; try omega.
+  destruct l; simpl; reflexivity.
+  assert (fuel = S k \/ k >= fuel)%nat by omega.
+  destruct H1. subst.
+  simpl. destruct l; simpl; auto.
+  f_equal. erewrite <- firstn_map. reflexivity.
+  replace (f a :: map f l) with (map f (a :: l)) by auto.
+  erewrite list_map_drop.
+  assert (n = O \/ n > O)%nat by omega.
+  destruct H1. subst. repeat erewrite get_each_n'_zero by (simpl; congruence).
+  eapply map_repeat_nil.
+  eapply IHlen.
+  eapply length_list_drop in H1.
+  instantiate (1 := a :: l) in H1.
+  omega. congruence.
+  instantiate (1 := k). omega.
+  eauto.
+Qed.
+
 Lemma get_each_n_map_commutes :
   forall {A B : Type} (f : A -> B) n l,
     get_each_n n (map f l) = map (map f) (get_each_n n l).
 Proof.
-  (* This is true, fun one though *)
-Admitted.
+  intros.
+  unfold get_each_n.
+  erewrite get_each_n'_map_commutes_strong.
+  erewrite map_length. reflexivity.
+  instantiate (1 := Datatypes.length l). omega.
+  instantiate (1 := Datatypes.length (map f l)). omega.
+Qed.
 
 Lemma strict_list_map_to_sval :
   forall l,
