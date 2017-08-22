9a316ebc74e6de3e32a1ba6218662250b388c3b8
Eric A Mullen <emullen@cs.washington.edu>
2017-08-22 16:50:49 -0700

    working on MessageSchedule

--- SHA256/MessageSchedule.v
+++ SHA256/MessageSchedule.v
@@ -24,5 +24,37 @@ Require Import Prims.
 Require Import SHA256.
 Require Import Helpers.
 
+(* TODO: *)
+Definition schedule_ev (e : ext_val) : ext_val := eseq nil.
 
+Lemma SHA256MessageSchedule_eval :
+  forall GE TE SE,
+    wf_env ge GE TE SE ->
+    forall a v res,
+      eager_eval_expr GE TE SE a (to_sval v) ->
+      has_type v (tseq 16 (tseq 32 tbit)) ->
+      res = to_sval (schedule_ev v) ->
+      eager_eval_expr GE TE SE (EApp (EVar SHA256MessageSchedule) a) res.
+Proof.
+  intros.
+  gen_global SHA256MessageSchedule.
+  gen_global (0,"demote").
+  gen_global (1,"+").
+  gen_global (2,"-").
+  gen_global (34,"#").
+  gen_global (40,"@").
+  gen_global (49,"fromTo").
+  inversion H1. subst v t.
+  e. ag.
+  all: clear H3.
+  e. e. g.
+  use append_eval.
+  lv.
+  e.
+  ec. ec.
+  e. e. e. ag.
+  (* TODO: fromTo *)
+
+
+Admitted.
 
